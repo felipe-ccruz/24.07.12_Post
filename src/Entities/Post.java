a6dc0dc6ad01993958c65a9f5d6cbe823527592b
Felipe Cruz <felipe23070305@aluno.cesupa.br>
2024-07-12 02:42:04 -0300

    Criação do projeto, juntamente com a criação das classe e a adição de cada atributo, getter, setter e construtor

---
+++ src/Entities/Post.java
@@ -0,0 +1,55 @@
+package Entities;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Post {
+    private LocalDateTime date;
+    private String title;
+    private String content;
+    private int likes;
+    private ArrayList<Comment> comments = new ArrayList<>();
+    public Post(String title, String content) {
+        this.date = LocalDateTime.now();
+        this.title = title;
+        this.content = content;
+    }
+    //MOTODS
+    
+
+
+    //GETTER & SETTER
+    public LocalDateTime getDate() {
+        return date;
+    }
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public int getLikes() {
+        return likes;
+    }
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+    public ArrayList<Comment> getComments(){
+        return comments;
+    }
+    public void addComment(Comment comment){
+        comments.add(comment);
+    } 
+    public void removeComment(Comment comment){
+        comments.remove(comment);
+    } 
+}
